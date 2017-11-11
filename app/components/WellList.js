156a01883d04b5ce452f1b61dcfc28d9015fd30c
gstq <ramazanov.anvar@gmail.com>
2017-11-11 18:58:57 +0500

    Add WellList component

--- app/components/WellList.js
+++ app/components/WellList.js
@@ -1,30 +1,19 @@
 import React, {Component}  from 'react'
 import {ListGroup, ListGroupItem} from 'react-bootstrap'
-import {db} from '../db.js'
+import {getWellList} from '../db.js'
 
 export default class WellList extends Component {
 
-    componentWillMount()
-    {
-        var well_items = [];
-        this.setState({well_items: well_items});
-        var that = this;
-        db.each("SELECT well_id as well_name, count(dyn_id) as count FROM dynamograms group by well_id", 
-            function(err, row) {
-                var well_item = {
-                    well_id: row.well_name,
-                    dyn_count: row.count
-                };
-                well_items.push(well_item);
-            },
-            function(){ 
-                that.setState({well_items: well_items});
+    componentWillMount(){
+        this.setState({well_items: [], selected_item: null});
+        getWellList().then(data => {
+            this.setState({well_items: data});
         });
     }
 
     render() {
         var wellItems = this.state.well_items.map((well_item) => {
-            return (<ListGroupItem>Скв. {well_item.well_id}</ListGroupItem>);
+            return (<ListGroupItem active={this.state.selected_item == well_item.well_id} onClick={() => this.setState({selected_item: well_item.well_id})}>Скв. {well_item.well_id}</ListGroupItem>);
         });
         return (<ListGroup>{wellItems}</ListGroup>)
     }
--- app/db.js
+++ app/db.js
@@ -1,2 +1,11 @@
-const sqlite3 = require('sqlite3').verbose();
-export const db = new sqlite3.Database('DYN_DATA.sqlite');
+import Promise from 'bluebird';
+import db from 'sqlite';
+
+const dbName = 'DYN_DATA.sqlite';
+
+export async function getWellList(){
+    const data = await Promise.resolve()
+        .then(() => db.open(dbName))
+        .then(() => db.all('SELECT well_id as well_id, count(dyn_id) as count FROM dynamograms group by well_id'))
+    return data;
+}
